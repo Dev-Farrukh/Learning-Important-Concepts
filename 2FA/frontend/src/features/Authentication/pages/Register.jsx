a6fe865c0aea6313317a9dd40b9de18a4f476040
dev-farrukh <m.farrukhnoman@gmail.com>
2026-09-03 05:54:28 -0700

    feat: add registration page with form validation and API integration

--- 2FA/frontend/src/features/Authentication/pages/Register.jsx
+++ 2FA/frontend/src/features/Authentication/pages/Register.jsx
@@ -1,19 +1,28 @@
 import { LockOutlined, UserOutlined, } from '@ant-design/icons';
 import { Button, Form, Input, notification } from 'antd';
 import { registerUser } from '../api/auth.api';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const [api , contextHolder] = notification.useNotification();
+    const navigate = useNavigate()
+    const [loading , setLoading] = useState(false)
 
     const onFinish = async (values) => {
+        setLoading(true)
         try {
             await registerUser(values)
+            api.success({title : "User created successfully"})
+            navigate("/login")
         } catch (error) {
-            console.log("ff",error);
+            console.log("ff",error.response.data.message);
             api.error({
                     title: 'Something went wrong',
-                    description : error.message
+                    description : error?.response?.data?.message
             });
+        } finally{
+            setLoading(false)
         }
     };
     return (
@@ -52,7 +61,7 @@ const Register = () => {
                 </Form.Item>
 
                 <Form.Item>
-                    <Button block type="primary" htmlType="submit" style={{ marginBottom: "10px" }}>
+                    <Button block type="primary" htmlType="submit" style={{ marginBottom: "10px" }} loading={loading}>
                         Register
                     </Button>
                     <div className="text-center text-sm text-zinc-400">
